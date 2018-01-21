357012e22d28d191a85566f9cb33da7fc6967aa6
Dmitry Suffi <dmitry.suffering@gmail.com>
2018-01-21 16:14:09 +0300

    test

---
+++ tests/Helpers/ConsoleHelperTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace suffi\Simple\tests\Helpers;
+
+use PHPUnit\Framework\TestCase;
+use suffi\Simple\Helpers\ConsoleHelper;
+
+class ConsoleHelperTest extends TestCase
+{
+
+    public function testSet()
+    {
+        $colorString = ConsoleHelper::color('text', '31');
+
+        $this->assertEquals($colorString, "\033[31m text \033[0m");
+    }
+
+}
